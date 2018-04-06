d11c7958d239960ebc5c48ed4d128e952f0bdbf3
Will Lewis <will@wflewis.com>
2018-04-06 14:28:40 -0400

    Restructure what gets saved

--- src/warscribe.cr
+++ src/warscribe.cr
@@ -3,7 +3,7 @@ require "json"
 
 module Warscribe
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
-  class_property recent_data : JSON::Any?
+  class_property recent_data : Hash(Symbol, String)?
 end
 
 server = Stout::Server.new(reveal_errors: true)
@@ -21,9 +21,16 @@ def handle(context)
 end
 
 def handle_post(context)
-  context << "thanks for making #holywars a better place"
+  Warscribe.recent_data = {
+    :user_name => context.data.not_nil!["user_name"].as_s,
+    :time      => Time.now.to_s,
+    :text      => context.data.not_nil!["text"].as_s,
+  }
+
+  context << "thanks for making #holywars a better place. now get back to fighting!"
   context << "\n"
-  Warscribe.recent_data = context.data
+rescue
+  context << "that's invalid data, stupid"
 end
 
 server.listen
