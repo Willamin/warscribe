e6851191565b69551be7aec6e5f8acb6161c4e2e
Will Lewis <will@wflewis.com>
2018-04-06 12:34:14 -0400

    Add some better echo functionality

--- src/warscribe.cr
+++ src/warscribe.cr
@@ -1,21 +1,29 @@
 require "stout"
+require "json"
 
 module Warscribe
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+  class_property recent_data : JSON::Any?
 end
 
 server = Stout::Server.new(reveal_errors: true)
 server.get("/read", &->handle(Stout::Context))
 server.post("/write", &->handle_post(Stout::Context))
 
+Warscribe.recent_data = nil
+
 def handle(context)
-  context << "Hello world"
+  if Warscribe.recent_data
+    context << Warscribe.recent_data.inspect
+  else
+    context << "nothing's been posted"
+  end
 end
 
 def handle_post(context)
-  context << "you posted"
+  context << "thanks for posting"
   context << "\n"
-  context << context.data.inspect
+  Warscribe.recent_data = context.data
 end
 
 server.listen
