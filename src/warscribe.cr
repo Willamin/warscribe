3c61df1f16f09716db2249945e19b81ed4356bbe
Will Lewis <will@wflewis.com>
2018-04-09 11:10:44 -0400

    add jerk restrictions

--- src/warscribe.cr
+++ src/warscribe.cr
@@ -8,25 +8,44 @@ module Warscribe
     api_key: ENV["AIRTABLE_API_KEY"],
     base: ENV["AIRTABLE_BASE_ID"]
   )
+
+  USER_TIMEOUT = Hash(String, Time).new
 end
 
 server = Stout::Server.new(reveal_errors: true)
 server.post("/write", &->handle(Stout::Context))
 
+class JerkException < Exception; end
+
+class AirException < Exception; end
+
 def handle(context)
+  now = Time.now
+  username = context.data.not_nil!["user_name"].as_s.strip
+
+  Warscribe::USER_TIMEOUT[username]?.try do |previous_submission_time|
+    submitting_too_fast = previous_submission_time - now < 1.minutes
+    raise JerkException.new if submitting_too_fast
+  end
+  Warscribe::USER_TIMEOUT[username] = now
+
   result = Warscribe::AIRTABLE.table("Wars").create(Airtable::Record.new({
-    "Submitter"     => context.data.not_nil!["user_name"].as_s.strip,
+    "Submitter"     => username,
     "Date Added"    => Time.now.to_s(Time::Format::ISO_8601_DATE_TIME.pattern).strip,
     "First Option"  => context.data.not_nil!["text"].as_s.split("vs")[0].strip,
     "Second Option" => context.data.not_nil!["text"].as_s.split("vs")[1].strip,
   }))
 
   if result.is_a? Airtable::Error
-    raise result.message
+    raise AirException.new
   end
 
   context << "thanks for making #holywars a better place. now get back to fighting!"
   context << "\n"
+rescue JerkException
+  context << "stop being a jerk. chill"
+rescue AirException
+  context << "something's wrong in the air"
 rescue
   context << "something didn't work... probably PEBCAK"
 end
